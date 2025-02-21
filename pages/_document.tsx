c286aa58d9dc21f8c64ed6cfbcdcc099bcc8e8c2
Tenzin <chaksamt@gmail.com>
2025-02-21 02:02:48 -0600

    logo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,19 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/static/PFS-optimized.svg" />
+        <meta charSet="UTF-8"></meta>
+        <meta
+          name="keywords"
+          content="Pons Ferreus Solutions provides digital solution consulting to companies."
+        ></meta>
+        <meta
+          name="description"
+          content="provides digital solution consulting to companies"
+        ></meta>
+        <meta name="author" content="Tenzin Chaksam"></meta>
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
